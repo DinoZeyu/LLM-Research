43dff0be1dd630606701f21bd7d7ae7c0a63ac15
DinoHan <hanzeyu89@gmail.com>
2024-11-26 19:35:21 -0500

    submission

--- Data/Finetuning Data/data_combine.py
+++ Data/Finetuning Data/data_combine.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd 
 
 linear_algebra_output = 'linear_algebra_finetuning_chat.jsonl'
 abstract_algebra_output = 'abstract_algebra_finetuning_chat.jsonl'
@@ -10,3 +11,20 @@ with open(combined_output, 'w') as combined_file:
         with open(file_name, 'r') as data_file:
             for line in data_file:
                 combined_file.write(line)
+
+
+csv_files = [
+    'Linear Algebra Calculation Data 1000.csv',
+    'Sythetic Abstract Algebra Data 3000.csv',
+    'Sythetic Linear Algebra Data 5000.csv'
+]
+
+dfs = []
+for file in csv_files:
+    df = pd.read_csv(file)
+    df['text'] = "Question: " + df['Question'] + " Answer: " + df['Answer']
+    dfs.append(df[['text']])  # Only keep the "text" column
+
+combined_df = pd.concat(dfs)
+combined_df.dropna(inplace=True)
+combined_df.to_csv("combined_finetuning_data.csv", index=False)
